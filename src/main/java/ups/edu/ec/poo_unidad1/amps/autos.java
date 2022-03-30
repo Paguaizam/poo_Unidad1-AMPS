7e9254b2664be49fef3646dfe48b5f63420f0d89
PC-08 <PC-08@DESKTOP-GRDGBOO>
2022-03-30 07:45:10 -0500

    Cambio del mensaje de hola mundo al español

---
+++ src/main/java/ups/edu/ec/poo_unidad1/amps/autos.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ups.edu.ec.poo_unidad1.amps;
+
+/**
+ *
+ * @author PC-08
+ */
+public class autos {
+    String marca;
+    int cilindraje;
+    String modelo;
+    double peso;
+}
+
