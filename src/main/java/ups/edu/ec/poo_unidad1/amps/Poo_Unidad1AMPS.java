51504f5f8ab29a8a96699ac52864ff109355cb59
PC-08 <PC-08@DESKTOP-GRDGBOO>
2022-03-30 06:33:54 -0500

    Cambio del mensaje de hola mundo al español

---
+++ src/main/java/ups/edu/ec/poo_unidad1/amps/Poo_Unidad1AMPS.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ups.edu.ec.poo_unidad1.amps;
+
+/**
+ *
+ * @author PC-08
+ */
+public class Poo_Unidad1AMPS {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+}
