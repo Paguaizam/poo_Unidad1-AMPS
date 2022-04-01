1c507b77825cf920d502783d8c8c9c5fab837191
PC-08 <PC-08@DESKTOP-GRDGBOO>
2022-04-01 06:59:32 -0500

    trabajo en clase

---
+++ src/main/java/ups/edu/ec/poo_unidad1/amps/computadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ups.edu.ec.poo_unidad1.amps;
+
+/**
+ *
+ * @author PC-08
+ */
+public class computadora {
+    String nombre;
+    String marca;
+    byte espacio_GB;
+    String procesador ; 
+}
