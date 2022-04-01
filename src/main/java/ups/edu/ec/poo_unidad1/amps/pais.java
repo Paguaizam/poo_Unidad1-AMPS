a99e25bf0d366e08b3353fb82654869f0b1d69d2
PC-08 <PC-08@DESKTOP-GRDGBOO>
2022-04-01 06:59:50 -0500

    trabajo en clase

---
+++ src/main/java/ups/edu/ec/poo_unidad1/amps/pais.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ups.edu.ec.poo_unidad1.amps;
+
+/**
+ *
+ * @author PC-08
+ */
+public class pais {
+    String nombre;
+    String capital;
+    long poblacion;
+    long tamaño; 
+}
